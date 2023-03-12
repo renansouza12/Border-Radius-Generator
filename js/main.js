f24d4538d345006a64e84169a892ced8c6357325
RenanSouza <renansouza0122@gmail.com>
2023-03-12 14:25:37 -0300

    function to copy border radius code added

--- js/main.js
+++ js/main.js
@@ -11,13 +11,23 @@ const btn_reset = document.querySelector("#reset");
 sliders.forEach((slider)=>{
     slider.addEventListener("input",()=>{
         changeBorderRadius();
+        showBorderRadiusCode();
     })
 })
+btn_copyCss.addEventListener("click",copyCodeCss);
 
 function changeBorderRadius(){
     box.style.borderTopLeftRadius = topLeft.value + '%';
     box.style.borderBottomLeftRadius = bottomLeft.value + '%';
     box.style.borderTopRightRadius = topRight.value + "%";
     box.style.borderBottomRightRadius = bottomRight.value + "%";
- 
+}
+
+function showBorderRadiusCode(){
+    css_code.innerHTML = `border-radius: ${topLeft.value}% ${topRight.value}% ${bottomLeft.value}% ${bottomRight.value}%`
+}
+
+async function copyCodeCss(){
+    const code = css_code.innerHTML;
+    await navigator.clipboard.writeText(code);
 }
